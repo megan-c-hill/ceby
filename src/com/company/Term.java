e49becfd89793513d1a9c08f8eb872063111fe0c
megan-c-hill <meganchill@iastate.edu>
2017-09-11 20:13:44 -0500

    Created methods for term class

--- src/com/company/Term.java
+++ src/com/company/Term.java
@@ -4,4 +4,43 @@ package com.company;
  * Created by megansISU on 9/11/17.
  */
 public class Term {
+    int coefficient;
+    int exponent;
+
+    public Term(int coefficient, int exponent){
+        this.coefficient = coefficient;
+        this.exponent = exponent;
+    }
+
+    public int getCoefficient(){
+        return coefficient;
+    }
+
+    public int getExponent(){
+        return exponent;
+    }
+
+    /**
+     * Multiplies this term by the given term
+     * @param t the given term
+     * @return the result of the multiplication
+     */
+    public Term multiply (Term t){
+        int newCoeff = t.getCoefficient() * this.getCoefficient();
+        int newExp = t.getExponent() + this.getExponent();
+        Term newTerm = new Term(newCoeff, newExp);
+        return newTerm;
+    }
+
+    public String toString(){
+        String result = "";
+        result += this.getCoefficient();
+        if (this.getExponent()!=0){
+            result += "x";
+            if(this.getExponent()!=1){
+                result += "^" + this.getExponent();
+            }
+        }
+        return result;
+    }
 }
